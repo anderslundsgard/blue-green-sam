c2a65c1228dbfde8051dbbf5dead27878d7c21e9
Anders Lundsgård <anders@utvecklare.eu>
2021-10-16 06:19:08 +0200

    Testing with Linear10PercentEvery1Minute deploy strategy

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
 
     print('Running Lambda with python 3.x!')
 
-    message = "Lokalise Everything!!"
+    message = "Lokalise Everything!! - Now with deploy strategy: Linear10PercentEvery1Minute"
 
     return {
         "statusCode": 200,
