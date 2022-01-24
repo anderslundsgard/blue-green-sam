1eef53757c18e14a2fa288299c1676f924e97c1b
Anders Lundsgård <anders@utvecklare.eu>
2022-01-24 16:10:22 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
 
     print('Running Lambda with python 3.x!')
 
-    message = "Lokalise Everything - Deploy strategy: AllAtOnce!"
+    message = "Lokalise Everything - Deploy strategy: AllAtOnce!!"
 
     return {
         "statusCode": 200,
